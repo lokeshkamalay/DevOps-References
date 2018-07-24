b238a996235434d0e971e518bcad4b6991ca58b1
Loki <lokesh.mydilse@gmail.com>
2018-07-23 22:46:12 -0500

    Update sample.groovy

--- jenkins/sample.groovy
+++ jenkins/sample.groovy
@@ -1,1 +1,11 @@
-node('agent1){
+node('agent1'){
+    stage('checkout'){
+        git credentialsId: 'gitaccess', url: 'https://github.com/lokeshkamalay/java-tomcat-maven-example.git'
+    }
+    stage('Build'){ //Creating the package, makesure you add mvn to path variables in agent
+        sh 'mvn clean package'
+    }
+    stage('Deploy'){ //deploying locally
+        sh 'mvn install -DskipTests=false'
+    }
+}
