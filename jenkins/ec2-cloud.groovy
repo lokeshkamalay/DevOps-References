d46553e6eaa4ca279350607d9395ffce5695f40e
Loki <lokesh.mydilse@gmail.com>
2019-03-19 10:13:23 -0500

    Added comments

--- jenkins/ec2-cloud.groovy
+++ jenkins/ec2-cloud.groovy
@@ -1,11 +1,11 @@
-node('ec2cloud-maven'){
+node('ec2cloud-maven'){ //the label coming from aws ec2 cloud
     stage('checkout'){
         git 'https://github.com/lokeshkamalay/simple-java-maven-app.git'
     }
     stage('Build'){
         sh "mvn clean package"
     }
-    stage('awscli'){
+    stage('awscli'){  //installing awscli in rhel machine; this stage can be skipped if you have chosen amazon linux or your ami has aws configured already.
         sh '''
         curl -O https://bootstrap.pypa.io/get-pip.py
         sudo python get-pip.py
@@ -13,12 +13,13 @@ node('ec2cloud-maven'){
         aws --version
         '''
     }
-    timeout(time: 5, unit: 'SECONDS'){
-        input message: 'Do Want To Deploy?', ok: 'Deploy'
+    timeout(time: 5, unit: 'SECONDS'){  //Timeout, in case no one takes action within stipulated time, the job will fail.
+        input message: 'Do Want To Deploy?', ok: 'Deploy'  //Halts the build until further action
     }
     stage('Copy to S3'){
         withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws-ec2-batch2-creds', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
-            sh '''
+            //withCredentials class, exports access and secret key into shell environment
+            sh '''  //when you want to execute multiple related shell commands, you need to run them in a block.
                 aws s3 cp target/*.jar s3://lokeshkamalay/
                 
                 if [[ $? -eq 0 ]]; then
@@ -30,6 +31,7 @@ node('ec2cloud-maven'){
         }
     }
     stage('Email'){
+        //emails the job details and the status.
         sh '''
             printenv
             mutt -s "Jenkins Job Status - $JOB_URL" lokesh.mydilse@gmail.com < result.txt
