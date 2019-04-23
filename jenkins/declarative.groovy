346bdb00e83fc2915bab057127a1cd1d0a771226
Loki <lokesh.mydilse@gmail.com>
2019-04-22 19:29:36 -0500

    Create declarative.groovy

---
+++ jenkins/declarative.groovy
@@ -0,0 +1,50 @@
+pipeline{
+    agent { None }
+    environment { 
+        WHEREAMI = "in Global Environment"
+    }
+    stages {
+        stage('Maven - 1'){
+            agent {
+                docker {
+                    image 'maven:latest'
+                    label 'docker' 
+                }
+            }
+            environment {
+                WHEREAMI = "In First stage"
+            }
+            steps {
+                sh "mvn --version"
+                sh "echo $WHEREAMI"
+            }
+        }
+
+        stage('Maven - 2'){
+            agent {
+                docker {
+                    image 'maven:3-jdk-8-alpine'
+                    label 'docker'
+                }
+            }
+            environment {
+                WHEREAMI = "In Second Stage"
+            }
+            steps {
+                sh "mvn --version"
+                sh "echo $WHEREAMI"
+            }
+        }
+    }
+    post {
+        always {
+            echo "I m in post $WHEREAMI"
+        }
+        failure {
+            echo "I failed"
+        }
+        success {
+            echo "mail me"
+        }
+    }
+}
