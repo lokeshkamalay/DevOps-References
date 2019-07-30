be0652211d6ebc518181c563045197692fe676e2
Loki <lokesh.mydilse@gmail.com>
2019-07-29 19:08:32 -0500

    Update aws.sh

--- bash/aws.sh
+++ bash/aws.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
 
-if [[ $# -ne 1 ]]; then
-	echo "This script except instance id"
-	exit 1
-fi
+echo "Getting the instance id here"
 
-echo "Current status of the instance $1"
-C_STATUS=$(aws ec2 describe-instances --instance-id $1 --query Reservations[].Instances[].State[].Name | grep -vE '\[|\]' | awk -F'"' '{ print $2 }')
+InstanceID=$(aws ec2 describe-instances --filter Name=tag:Name,Values=sq.com --query Reservations[].Instances[].InstanceId | grep -vE '\[|\]' | awk -F'"' '{ print $2 }')
+
+echo "Current status of the instance $InstanceID"
+C_STATUS=$(aws ec2 describe-instances --instance-id $InstanceID --query Reservations[].Instances[].State[].Name | grep -vE '\[|\]' | awk -F'"' '{ print $2 }')
 
 if [[ $C_STATUS == "running" ]]; then
-	echo "The instance is running, stopping it now"
-	aws ec2 stop-instances --instance-ids $1
+        echo "The instance is running, stopping it now"
+        aws ec2 stop-instances --instance-ids $InstanceID
         if [[ $? == 0 ]];then
-		echo "Instance is stopped successfully"
-	else
-		echo "There seems to be a problem in stopping the instance, pls do it manually"
-	fi
+                echo "Instance is stopped successfully"
+        else
+                echo "There seems to be a problem in stopping the instance, pls do it manually"
+        fi
 else
-	echo "The machine is not in running status"
+        echo "The machine is not in running status"
 fi
