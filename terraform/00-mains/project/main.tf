a0b8aaba86f4ca6e5b4448fb06103e29c30d082f
Loki <lokesh.mydilse@gmail.com>
2021-01-31 09:28:02 -0600

    Update main.tf

--- terraform/00-mains/project/main.tf
+++ terraform/00-mains/project/main.tf
@@ -3,6 +3,15 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+# Storing the state file safely in a remote location that is accessible to everyone
+terraform {
+  backend "s3" {
+    bucket = "lokeshkamalay"
+    key    = "terraform/hr.tfstate"
+    region = "us-east-1"
+  }
+}
+
 module "project" {
   source  = "../../10-module/project"
   appname = "TerrafromDemo"
